952eb3a3ef060f8f03bfe0460b9248d03a1cac57
almusto <ales.musto@gmail.com>
2017-02-21 09:02:44 -0500

    protocols

--- Hospital/SchoolEmployees.swift
+++ Hospital/SchoolEmployees.swift
@@ -4,7 +4,39 @@
 
 
 
+struct AdvisoryBoardMember {
+  var name: String
+  var vacationDays: Int = 30
+  func wages() -> Double {
+    return 100_000.00
+  }
+}
 
+extension AdvisoryBoardMember : Employee {
+  //don't have to do anyhting as we've already implemented wages and vacationdays in our struct and the other methods provide default values
+}
+
+struct Principal {
+  var name: String
+  var vacationDays: Int = 20
+  func wages() -> Double {
+    return 80_000.00
+  }
+}
+
+extension Principal: Employee, Reprimand {
+
+}
+
+
+struct Teacher {
+  var name: String
+  var vacationDays: Int = 15
+}
+
+extension Teacher: Teach, Employee {
+  
+}
 
 
 
